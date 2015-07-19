93383663640a0ca8da683dee0bc35da951974f8c
J3698 <antiochsanders@gmail.com>
2015-07-18 22:55:03 -0400

    -TwoP.java extra imports removed

--- TwoP.java
+++ TwoP.java
@@ -1,21 +1,10 @@
 import javax.swing.JPanel;
 import javax.swing.JFrame;
-import javax.swing.Timer;
-import javax.swing.ImageIcon;
-import java.awt.Graphics;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.image.BufferedImage;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.Random;
 
 public class TwoP {
    public static int gameWidth = 640;
    public static int gameHeight = 480;
-   
+
    public static void main(String[] args) {
       JFrame frame = new JFrame("TwoP");
       frame.setSize(gameWidth, gameHeight);
