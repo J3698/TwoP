f2a53f460c8a7696f5a3f2a425ff9e8212710328
Antioch Sanders <antiochsanders@gmail.com>
2015-07-21 13:05:55 -0400

    Instructions gameMode in GamePanel
    -Instructions triggers, not implemented
    -Extraneous comment deletes

--- GamePanel.java
+++ GamePanel.java
@@ -11,6 +11,7 @@ import java.awt.Color;
 import java.lang.Thread;
 
 public class GamePanel extends JPanel {
+   Instructions myInstructions = new Instructions();
    Play myPlay = new Play();
    Pause myPause = new Pause();
    GameOver myGameOver = new GameOver();
@@ -20,7 +21,7 @@ public class GamePanel extends JPanel {
    int myGameHeight;
    Player firstPlayer;
    Player secondPlayer;
-   String gameMode = "play"; //Why doesn't this work if its "gameover"?
+   String gameMode = "play";
    Font playAndResumeFont;
    Font scoreFont;
    Timer timer;
@@ -54,6 +55,7 @@ public class GamePanel extends JPanel {
       timer.start();
    }
    public void update() {
+      myInstructions.checkUpdateTrigger(gameMode);
       myPlay.checkUpdateTrigger(gameMode);
       myPause.checkUpdateTrigger(gameMode);
       myGameOver.checkUpdateTrigger(gameMode);
@@ -61,12 +63,12 @@ public class GamePanel extends JPanel {
    public void paintComponent(Graphics pen) {
       pen.clearRect(0, 0, getWidth(), getHeight());
       myBuffer.clearRect(0, 0, myGameWidth, myGameHeight);
+      myInstructions.checkDrawTrigger(gameMode, myBuffer);
       myPlay.checkDrawTrigger(gameMode, myBuffer);
       myPause.checkDrawTrigger(gameMode, myBuffer);
       myGameOver.checkDrawTrigger(gameMode, myBuffer);
       pen.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
    }
-
    public class UpdateListener implements ActionListener {
       public void actionPerformed(ActionEvent event) {
          update();
@@ -75,6 +77,7 @@ public class GamePanel extends JPanel {
    }
    public class KeyListener extends KeyAdapter {
       public void keyPressed(KeyEvent event) {
+         myInstructions.checkKeyListenTrigger(event);
          myPlay.checkKeyListenTrigger(event);
          myPause.checkKeyListenTrigger(event);
          myGameOver.checkKeyListenTrigger(event);
@@ -94,41 +97,41 @@ public class GamePanel extends JPanel {
       void keyListen(KeyEvent event);
    }
 
-   public class Load implements GameState {
-      private String myGameMode = "load";
-      private String myNextMode;
-      Thread myLoader;
-      
-      public Load(Thread loader, String nextMode) {
-         myLoader = loader;
-         myNextMode = nextMode;
-      }
-
-      public void run() {
-         myLoader.run();
-      }
+   public class Instructions implements GameState {
+      private String myGameMode = "instructions";
 
       public void checkDrawTrigger(String gameMode, Graphics pen) {
          if (myGameMode == gameMode)
             draw(pen);
       }
       public void draw(Graphics pen) {
-
+         pen.setColor(Color.white);
+         pen.fillRect(0, 0, myGameWidth, myGameHeight);
+         pen.setColor(Color.red);
+         pen.drawString("w", 10, 120);
+         pen.drawString("a", 20, 110);
+         pen.drawString("s", 30, 100);
+         pen.drawString("d", 40, 90);
+         pen.drawString("up", 50, 80);
+         pen.drawString("left", 60, 70);
+         pen.drawString("down", 70, 60);
+         pen.drawString("right", 80, 50);
+         pen.drawString("1", 90, 40);
+         pen.drawString("2", 100, 30);
+         pen.drawString(".", 110, 20);
+         pen.drawString("/", 120, 10);
       }
       public void checkUpdateTrigger(String gameMode) {
          if (myGameMode == gameMode)
             update();
       }
       public void update() {
-         if (myLoader.getState() == Thread.State.TERMINATED)
-            gameMode = myNextMode;
       }
       public void checkKeyListenTrigger(KeyEvent event) {
          if (myGameMode == gameMode)
             keyListen(event);
       }
       public void keyListen(KeyEvent event) {
-
       }
    }
 
@@ -136,7 +139,7 @@ public class GamePanel extends JPanel {
       private String myGameMode = "play";
       private int myTextOpacity = 50;
       public void draw(Graphics pen) {
-         drawBackground(myBuffer);  //drawGame();
+         drawBackground(myBuffer);
          drawPlayers(myBuffer);
          drawGUI(myBuffer);
       }
