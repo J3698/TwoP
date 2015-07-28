e13a748fe2d4cd54386435a6c806aa516f85ee7d
Antioch Sanders <antiochsanders@gmail.com>
2015-07-28 14:44:20 -0400

    Changes to Bumper.java
    -Variable name clarifications/fixes
    -No longer implements Observer
    -Constructor changed, super added
    -Update changed so players will be bounced off

--- Bumper.java
+++ Bumper.java
@@ -1,33 +1,50 @@
-public class Bumper extends Circle implements Observer {
+import java.awt.Color;
+
+public class Bumper extends Circle {
+   private double myStrength;
    private int myMorphTime;
-   private int myMatureLifetime;
+   private int myMaturationTime;
    private ColorCross myColorCross;
    private int myAge = 0;
    /**
     *
     *
     */
-    public Bumper(Vector2 position, double width, double height,
-                   Color background, Color color, int morphTime,
-                                                 maturationTime) {
-      super(position, width, height);
+    public Bumper(Vector2 position, double radius, Color background,
+                     Color color, int morphTime, int maturationTime) {
+      super(position, radius);
       setColor(color);
       setOutline(color);
       myColorCross = new ColorCross(background, color, 1);
       myMorphTime = morphTime;
       myMaturationTime = maturationTime;
+      myStrength = 8;
    }
 
-   public void update() {
+   public void update(Player firstPlayer, Player secondPlayer) {
+      Vector2 bumpForce;
+      if (collidesCircle(firstPlayer)) {
+         bumpForce = firstPlayer.getCenter().copy();
+         bumpForce.subtractVector(getCenter());
+         bumpForce.setMagnitude(myStrength);
+         firstPlayer.applyForce(bumpForce);
+      }
+      if (collidesCircle(secondPlayer)) {
+         bumpForce = secondPlayer.getCenter().copy();
+         bumpForce.subtractVector(getCenter());
+         bumpForce.setMagnitude(myStrength);
+         secondPlayer.applyForce(bumpForce);
+      }
+ 
       if (myAge <= myMorphTime) {
          //Growth time
-         setAllColor(myColorCross.getPercent((myAge + 0.0) / morphTime));
+         setAllColor(myColorCross.getPercent((myAge + 0.0) / myMorphTime));
       }else if (myAge <= myMaturationTime + myMorphTime) {
          //Stable time
       }else if (myAge <= myMaturationTime + 2 * myMorphTime) {
          //Aging time
          setAllColor(myColorCross.getPercent((myAge + 0.0) /
-                        (myMaturationTime + 2 * morphTime)));
+                        (myMaturationTime + 2 * myMorphTime)));
       }else {
          //Die
 
