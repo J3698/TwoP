c31cb63e043517c257fff04121c5e12cd72ea3b3
Antioch Sanders <antiochsanders@gmail.com>
2015-07-29 16:09:06 -0400

    -Pause time to fade shorter in Game.java

--- Plane.java
+++ Plane.java
@@ -1,23 +1,20 @@
 import java.awt.Color;
 
 public class Plane extends Rect {
-   private int myMorphTime;
-   private int myMaturationTime;
    private ColorCross myColorCross;
+   private int myMorphTime = 200;
+   private int myMaturationTime = 200;
    private int myAge = 0;
    /**
     *
     *
     */
     public Plane(Vector2 position, double width, double height,
-                   Color background, Color color, int morphTime,
-                                             int maturationTime) {
+                                 Color background, Color color) {
       super(position, width, height);
       setColor(color);
       setOutline(color);
       myColorCross = new ColorCross(background, color, 1);
-      myMorphTime = morphTime;
-      myMaturationTime = maturationTime;
    }
 
    public void update(Player firstPlayer, Player secondPlayer) {
@@ -34,6 +31,12 @@ public class Plane extends Rect {
       myAge++;
    }
    
+   public void setMorphTime(int morphTime) {
+      myMorphTime = morphTime;
+   }
+   public void setmyMaturationTime(int maturationTime) {
+      myMaturationTime = maturationTime;
+   }
    public Effect getEffect(Player player) {
       return new Effect(player);
    }
