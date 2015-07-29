667d5f463f38a97f48caca4a45e6a4052ffe88ac
Antioch Sanders <antiochsanders@gmail.com>
2015-07-29 00:56:27 -0400

    PlaneHandler.java major changes (sorta works)
    -New imports
    -myGameWidth/myGameHeight fields
    -Random plane generation added (primitive)
    -drawPlanes method now just draw

--- PlaneHandler.java
+++ PlaneHandler.java
@@ -1,18 +1,31 @@
 import java.awt.Graphics;
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PlaneHandler {
+   private int myGameWidth;
+   private int myGameHeight;
    private ArrayList<Plane> planes;
+   private Random myRandom;
 
-   public PlaneHandler() {
+   public PlaneHandler(int gameWidth, int gameHeight) {
+      myGameHeight = gameHeight;
+      myGameWidth = gameWidth;
       planes = new ArrayList<Plane>();
+      myRandom = new Random();
    }
 
    public void update() {
+      if (myRandom.nextInt(1000) == 44) {
+         planes.add(new FirePlane(new Vector2(myGameWidth / 2, myGameHeight - 10),
+                                                   30, 10, Color.green, 200, 200));
+         System.out.println(",,,,....");
+      }
       for (Plane p: planes)
          p.update();
    }
-   public void drawPlanes(Graphics pen) {
+   public void draw(Graphics pen) {
       for (Plane p: planes)
          p.draw(pen);
    }
