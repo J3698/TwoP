87b605124c967142806cf9d7ab71631f87cbb117
Antioch Sanders <antiochsanders@gmail.com>
2015-07-31 15:20:27 -0400

    -Player.java no infinite health

--- Player.java
+++ Player.java
@@ -13,7 +13,7 @@ public class Player extends Circle {
    private static double gravity = -0.5;
    private Controls myControls = new Controls(this);
    private ArrayList<Effect> myEffects = new ArrayList<Effect>();
-   private double myHealth = 200000000;
+   private double myHealth = 300;
    private Vector2 myVelocity;
    private double myAcceleration = 1;
    private double mySpeed = 0;
@@ -61,7 +61,7 @@ public class Player extends Circle {
       keepInBounds();
       updateJumpAbility();
       myGun.update();
-      
+
       Effect effect;
       for (int index = 0; index < myEffects.size(); index++) {
          effect = myEffects.get(index);
