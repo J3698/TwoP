a92ff9dfc1941ea5ab390c2625e0ac745cde4e3e
Antioch Sanders <antiochsanders@gmail.com>
2015-07-31 15:15:06 -0400

    FireEffect.java changes
    -Longer lifespan
    -Does more damage
    -Actually wears off

--- FireEffect.java
+++ FireEffect.java
@@ -4,14 +4,15 @@ public class FireEffect extends Effect {
    private ParticleSystem myVisualEffect;
 
    public FireEffect(Player player) {
-      super(player, 300);
+      super(player, 350);
       myVisualEffect = new ParticleSystem(getPlayer().getCenter(), "fire");
    }
 
    public void update() {
-      getPlayer().applyDamage(0.001);
+      getPlayer().applyDamage(0.01);
       myVisualEffect.setSourcePosition(getPlayer().getCenter().copy());
       myVisualEffect.update();
+      loseLife(1);
    }
    public void draw(Graphics pen) {
       myVisualEffect.draw(pen);
