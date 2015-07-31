a08ca5cf3a3b36841a1ff1939a1e9a30d3dca10a
Antioch Sanders <antiochsanders@gmail.com>
2015-07-31 15:13:49 -0400

    -Cleanup counts/prints lines of java

--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,7 @@ import os
 import time
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
+line_total = 0
 print("Removed files:")
 print("--------")
 for f in files:
@@ -9,7 +10,11 @@ for f in files:
         if f[-6:] == ".class":
             print("   "+f)
             os.remove(f)
+        if f[-5:] == ".java":
+            m = open(f, "r")
+            line_total += len(m.readlines())
     except IndexError:
         continue
 print("--------")
-time.sleep(7);
+print(str(line_total) + " lines of .java counted.")
+time.sleep(5);
