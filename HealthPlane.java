bab5e8b6b4fe31a2b86b08d80fb7f3935297c145
Antioch Sanders <antiochsanders@gmail.com>
2015-07-31 22:17:34 -0400

    -tick added for health plane, will be delay for health given

--- HealthPlane.java
+++ HealthPlane.java
@@ -1,9 +1,16 @@
 import java.awt.Color;
 
 public class HealthPlane extends Plane {
+   public static int myTick = 0;
+
    public HealthPlane(Vector2 position, double width, double height,
                                          Color background, int spot) {
       super(position, width, height, background, Color.white, spot);
+   }
+
+   public void update(Player firstPlayer, Player secondPlayer) {
+      super.update(firstPlayer, secondPlayer);
+      myTick++;
    }
 
    public Effect getEffect(Player player) {
