d167a17871eaf981a965becc7ed76d43d58600d3
J3698 <antiochsanders@gmail.com>
2015-08-02 01:22:39 -0400

    -PoisonEffect.java, PoisonVisual uncommented

--- PoisonEffect.java
+++ PoisonEffect.java
@@ -1,18 +1,21 @@
 import java.awt.Graphics;
 
 public class PoisonEffect extends Effect {
-   //private PoisonParticleSystem myPoisonVisual
+   private ParticleSystem myPoisonVisual;
 
    public PoisonEffect(Player player) {
       super(player, 300);
-      //myPoisonVisual = new ___;
+      myPoisonVisual = new ParticleSystem(player.getCenter(), "poison");
+      myPoisonVisual.setEmissionSkip(5);
    }
 
    public void update() {
       getPlayer().applyDamage(0.001);
-      //myPoisonVisual.update();
+      myPoisonVisual.setSourcePosition(getPlayer().getCenter().copy());
+      myPoisonVisual.update();
+      loseLife(1);
    }
    public void draw(Graphics pen) {
-      //myPoisonVisual.draw(pen);
+      myPoisonVisual.draw(pen);
    }
 }
