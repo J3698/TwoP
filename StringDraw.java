9a49c21059f0dc1437fc4bd14ca2e4fb67bbf939
Antioch Sanders <antiochsanders@gmail.com>
2015-08-04 10:54:04 -0400

    -StringDraw.java, for managing fonts/drawing text

---
+++ StringDraw.java
@@ -0,0 +1,12 @@
+import java.awt.Font;
+
+public class StringDraw {
+
+   public static Font playResumeFont() {
+      return new Font("Ariel", Font.BOLD, 90);
+   }
+
+   public static Font versionFont() {
+      return new Font("Sans", 1, 14);
+   }
+}
