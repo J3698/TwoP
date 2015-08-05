4f7232b38ca7076f4038bb8bc79a6cfc33faffcb
Antioch Sanders <antiochsanders@gmail.com>
2015-08-05 15:38:13 -0400

    Changes to ItemHandler.java
    -Import and package name fixes
    -Constructor name fixed
    -Draw and update implemented

--- handler/ItemHandler.java
+++ handler/ItemHandler.java
@@ -1,12 +1,24 @@
+package twop.handler;
+
+import twop.item.*;
+import twop.Player;
+
+import java.awt.Graphics;
+
 public class ItemHandler {
    private int myGameWidth;
    private int myGameHeight;
    private ItemFactory myItemFactory;
 
-   public DropHandler(int gameWidth, int gameHeight) {
+   public ItemHandler(int gameWidth, int gameHeight) {
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
       myItemFactory = new ItemFactory();
+   }
+
+   public void draw(Graphics pen) {
+   }
+   public void update(Player firstPlayer, Player secondPlayer) {
    }
 
    //Implement different sorts of entrances, e.g. from explosion of
