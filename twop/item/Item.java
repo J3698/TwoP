2576ad3375f0a37f62d9afa5dad451149127f67e
J3698 <antiochsanders@gmail.com>
2015-08-06 13:08:15 -0400

    Changes to Item.java interface
    -Imports added
    -Required functions added to
    ...Issues caused by getWidth/Height etc. requirements, as some things
    extend circle instead of rect, where there is a radius.
    ...Plan to either rewrite circle and all associated stuff, or delete
    requirements

--- twop/item/Item.java
+++ twop/item/Item.java
@@ -1,10 +1,18 @@
 package twop.item;
 
 import twop.Player;
+import twop.util.Vector2;
+import twop.Circle;
 
 import java.awt.Graphics;
 
 public interface Item {
    void draw(Graphics pen);
    void update(Player firstPlayer, Player secondPlayer);
+   boolean collidesCircle(Circle circle);
+   Vector2 getPosition();
+   void setPosition(Vector2 position);
+   boolean isDead();
+   double getWidth();
+   double getHeight();
 }
