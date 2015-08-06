c8b64f270e8accf0045d4840374ee13267528d8b
J3698 <antiochsanders@gmail.com>
2015-08-06 00:17:45 -0400

    Organizational changes, Item related changes
    -jGRASP project
    -HealthPack.java created
    -ItemHandler further implemented

--- twop/handler/ItemHandler.java
+++ twop/handler/ItemHandler.java
@@ -4,21 +4,28 @@ import twop.item.*;
 import twop.Player;
 
 import java.awt.Graphics;
+import java.util.Random;
+import
 
 public class ItemHandler {
    private int myGameWidth;
    private int myGameHeight;
+   private Random myRandom
+   private ArrayList<Item> myItems;
    private ItemFactory myItemFactory;
 
    public ItemHandler(int gameWidth, int gameHeight) {
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
+      myRandom = new Random();
+      myItems = new ArrayList<Item>();
       myItemFactory = new ItemFactory();
    }
 
    public void draw(Graphics pen) {
    }
    public void update(Player firstPlayer, Player secondPlayer) {
+
    }
 
    //Implement different sorts of entrances, e.g. from explosion of
---
+++ twop/item/HealthPack.java
@@ -0,0 +1,12 @@
+package twop.item;
+
+import twop.Circle;
+
+import java.awt.Graphics;
+
+public class HealthPack extends Circle implements Item {
+   public HealthPack(Vector2 position) {
+   
+   }
+
+}
