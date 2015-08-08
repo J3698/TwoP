b1a479dddaee33296e9ed416ed1762e9140b26fd
J3698 <antiochsanders@gmail.com>
2015-08-08 01:56:27 -0400

    Changes to Effect.java
    -myIntensity int, helper methods
    -empty onDeath method header
    -helper methods rewritten to conserve space

--- twop/effect/Effect.java
+++ twop/effect/Effect.java
@@ -5,8 +5,9 @@ import twop.Player;
 import java.awt.Graphics;
 
 public abstract class Effect {
-   private int myLife;
    private Player myPlayer;
+   private int myLife;
+   private int myIntensity = 1;
 
    public Effect(Player player, int life) {
       myPlayer = player;
@@ -15,27 +16,21 @@ public abstract class Effect {
 
    public abstract void update();
    public abstract void draw(Graphics pen);
+   public void onDeath() {}
 
-   public void increaseIntensity() {}
-
-   public Player getPlayer() {
-      return myPlayer;
-   }
-   public void setPlayer(Player player) {
-      myPlayer = player;
-   }
    public boolean isDead() {
       if (myLife <= 0)
          return true;
       return false;
    }
-   public void loseLife(int damage) {
-      myLife -= damage;
-   }
-   public int getLife() {
-      return myLife;
-   }
-   public void setLife(int life) {
-      myLife = life;
-   }
+
+   public Player getPlayer() { return myPlayer; }
+   public void setPlayer(Player player) { myPlayer = player; }
+   public void loseLife(int damage) { myLife -= damage; }
+   public int getLife() { return myLife; }
+   public void setLife(int life) { myLife = life; }
+   public void increaseIntensity() { myIntensity++; }
+   public int getIntensity() { return myIntensity; }
+   public void setIntensity(int intensity) { myIntensity = intensity; }
+
 }
