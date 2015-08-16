2d045b9dcf255609de13d97d04c04602671c4bb5
J3698 <antiochsanders@gmail.com>
2015-08-16 13:55:48 -0400

    -MenuButton.java started

---
+++ twop/gui/MenuButton.java
@@ -0,0 +1,13 @@
+package twop.gui;
+
+
+
+import java.awt.Graphics;
+
+public class MenuButton extends Button {
+   public MenuButton() {
+      super();
+   }
+
+   public void draw(Graphics pen) {}
+}
