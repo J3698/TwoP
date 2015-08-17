eff43d99ffe1cc23c2519f6178baedf3bd5a044a
Antioch Sanders <antiochsanders@gmail.com>
2015-08-16 21:58:56 -0400

    GUIManager supports Buttons
    -import change
    -Loops through buttons checking for button presses

--- twop/gui/GUIManager.java
+++ twop/gui/GUIManager.java
@@ -2,6 +2,7 @@ package twop.gui;
 
 import java.util.ArrayList;
 
+import java.awt.event.MouseEvent;
 import java.awt.Graphics;
 
 public class GUIManager {
@@ -14,6 +15,16 @@ public class GUIManager {
    public void draw(Graphics pen) {
       for (Button button: myButtons)
          button.draw(pen);
+   }
+
+   public void mouseListen(MouseEvent event) {
+      if (event.getButton() == MouseEvent.BUTTON1) {
+         for (Button button: myButtons) {
+            if (button.collidesPoint(event.getX() * 480 / 580, event.getY() * 640 / 770))
+               button.doAction();
+         }
+      }
+
    }
 
    public void addButton(Button button) {
