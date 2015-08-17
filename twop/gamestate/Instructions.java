6fc6147f6d04ee6ba648d181cc5ae943afce2a01
Antioch Sanders <antiochsanders@gmail.com>
2015-08-17 19:19:20 -0400

    Changes to Instructions.java
    -import changes
    -next, mainMenu, back buttons
    -pages of instructions implemented

--- twop/gamestate/Instructions.java
+++ twop/gamestate/Instructions.java
@@ -2,12 +2,16 @@ package twop.gamestate;
 
 import twop.Game;
 import twop.util.StringDraw;
+import twop.util.Vector2;
 import twop.gui.GUIManager;
+import twop.gui.InstructionsButton;
+
 
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -18,43 +22,91 @@ public class Instructions extends GameState {
    private GUIManager myGUIManager;
    private KeyAdapter myKeyListener;
    private MouseAdapter myMouseListener;
+   private int myCurrentPage;
 
    public Instructions(Game game, int gameWidth, int gameHeight) {
       super("instructions");
       myGame = game;
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
+      myGUIManager = new GUIManager();
       myMouseListener = new MouseListener();
       myKeyListener = new KeyListener();
-      myGUIManager = new GUIManager();
+      myCurrentPage = 1;
+      myGUIManager.addButton(new InstructionsButton(new BackListener(), "Back", new Vector2(20, 420), myGameWidth, myGameHeight));
+      myGUIManager.addButton(new InstructionsButton(new NextListener(), "Next", new Vector2(480, 420), myGameWidth, myGameHeight));
+      myGUIManager.addButton(new InstructionsButton(new MainMenuListener(), "MainMenu", new Vector2(250, 420), myGameWidth, myGameHeight));
    }
 
-   public void mouseListen(MouseEvent event) {}
-   public void keyListen(KeyEvent event) {}
    public void update() {}
+
    public void draw(Graphics pen) {
       pen.setColor(new Color(100, 100, 100));
       pen.fillRect(0, 0, myGameWidth, myGameHeight);
-      firstPage(pen);
+
+      switch (myCurrentPage){
+         case 1: firstPage(pen);
+            break;
+         case 2: secondPage(pen);
+            break;
+         case 3: thirdPage(pen);
+            break;
+      }
+
+      myGUIManager.draw(pen);
    }
 
    public void firstPage(Graphics pen) {
-      pen.setColor(new Color(0, 30, 100));
+      pen.setColor(Color.white);
       StringDraw.drawStringCenter(pen, "Welcome to TwoP, the simple two player bash 'em up game!", myGameWidth / 2, myGameHeight / 2);
    }
 
-   public void secondPage(Graphics pen) {}
+   public void secondPage(Graphics pen) {
+      pen.setColor(Color.white);
+      StringDraw.drawStringCenter(pen, "Read the second page here!", myGameWidth / 2, myGameHeight / 2);
+   }
 
-   public void thirdPage(Graphics pen) {}
+   public void thirdPage(Graphics pen) {
+      pen.setColor(Color.white);
+      StringDraw.drawStringCenter(pen, "Press next to return to the main menu!", myGameWidth / 2, myGameHeight / 2);
+   }
+
+   public class BackListener implements ActionListener {
+      public void actionPerformed(ActionEvent event) {
+         if (myCurrentPage == 1) {
+            myGame.setGameMode("mainmenu");
+            myCurrentPage = 1;
+         } else
+            myCurrentPage--;
+      }
+   }
+
+   public class NextListener implements ActionListener {
+      public void actionPerformed(ActionEvent event) {
+         if (myCurrentPage == 3) {
+            myGame.setGameMode("mainmenu");
+            myCurrentPage = 1;
+         } else
+            myCurrentPage++;
+      }
+   }
+
+   public class MainMenuListener implements ActionListener {
+      public void actionPerformed(ActionEvent event) {
+         myGame.setGameMode("mainmenu");
+         myCurrentPage = 1;
+      }
+   }
 
    public KeyAdapter getKeyListener() { return myKeyListener; }
    public MouseAdapter getMouseListener() { return myMouseListener; }
 
    private class KeyListener extends KeyAdapter {
-	   
    }
 
    private class MouseListener extends MouseAdapter {
-	   
+      public void mousePressed(MouseEvent event) {
+         myGUIManager.mousePressed(event);
+      }
    }
 }
