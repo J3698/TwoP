986f532925b939db03173be22285648a30c01ae3
Antioch Sanders <antiochsanders@gmail.com>
2015-08-17 19:23:04 -0400

    Changes to Button.java, has much of reponsibilities first in MenuButton
    -Import changes
    -Variable changes
    -Constructor changed
    -Draw implemented
    -Some getters/setters

--- twop/gui/Button.java
+++ twop/gui/Button.java
@@ -1,9 +1,12 @@
 package twop.gui;
 
+import twop.util.StringDraw;
 import twop.util.Vector2;
 
 import java.awt.event.ActionListener;
 import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.Color;
 
 public abstract class Button {
    private ActionListener myListener;
@@ -11,17 +14,35 @@ public abstract class Button {
    private int myGameHeight;
    private Vector2 myPosition;
    private Vector2 mySize;
+   private String myText;
+   private Color myBodyColor;
+   private Color myTextColor;
+   private Font myFont;
 
-   public Button(ActionListener listener, Vector2 position, Vector2 size,
-                                            int gameWidth, int gameHeight) {
+   public Button(ActionListener listener, String text, Color bodyColor, Color textColor, Font font,
+                                     Vector2 position, Vector2 size, int gameWidth, int gameHeight) {
       myListener = listener;
-      myGameWidth = gameWidth;
-      myGameHeight = gameHeight;
+      myText = text;
+      myFont = font;
+      myBodyColor = bodyColor;
+      myTextColor = textColor;
       myPosition = position;
       mySize = size;
+      myGameWidth = gameWidth;
+      myGameHeight = gameHeight;
    }
 
-   public abstract void draw(Graphics pen);
+   public void draw(Graphics pen) {
+      pen.setColor(myBodyColor);
+      int x = (int) myPosition.getX();
+      int y = (int) myPosition.getY();
+      int width = (int) mySize.getX();
+      int height = (int) mySize.getY();
+      pen.fillRect(x, y, width, height);
+      pen.setColor(myTextColor);
+      pen.setFont(myFont);
+      StringDraw.drawStringCenter(pen, myText, x + width / 2, y + height / 2);
+   }
 
    public void doAction() {
       myListener.actionPerformed(null);
@@ -35,4 +56,6 @@ public abstract class Button {
 
    public Vector2 getPosition() { return myPosition; }
    public Vector2 getSize() { return mySize; }
+   public void setBodyColor(Color bodyColor) { myBodyColor = bodyColor; }
+   public void setTextColor(Color textColor) { myTextColor = textColor; }
 }
