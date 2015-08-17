a48f481c23d8e11b70935817892ab1eedfd5dbed
Antioch Sanders <antiochsanders@gmail.com>
2015-08-16 21:51:28 -0400

    GameState abstract, not interface, implements trigger checks

--- twop/gamestate/GameState.java
+++ twop/gamestate/GameState.java
@@ -2,18 +2,42 @@ package twop.gamestate;
 
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
-
+import java.awt.event.MouseEvent;
 
 /**
  * Interface that different game states should
  * implement.
  *
  */
-public interface GameState {
-   void checkDrawTrigger(String myCurrentGameMode, Graphics pen);
-   void draw(Graphics pen);
-   void checkUpdateTrigger(String myCurrentGameMode);
-   void update();
-   void checkKeyListenTrigger(String currentGameMode, KeyEvent event);
-   void keyListen(KeyEvent event);
+public abstract class GameState {
+   public String myGameMode;
+
+   public GameState(String gameMode) {
+      myGameMode = gameMode;
+   }
+
+   public abstract void draw(Graphics pen);
+   public abstract void update();
+   public abstract void keyListen(KeyEvent event);
+   public abstract void mouseListen(MouseEvent event);
+
+   public void checkDrawTrigger(String currentGameMode, Graphics pen) {
+      if (myGameMode == currentGameMode)
+         draw(pen);
+   }
+
+   public void checkUpdateTrigger(String currentGameMode) {
+      if (myGameMode == currentGameMode)
+         update();
+   }
+
+   public void checkKeyListenTrigger(String currentGameMode, KeyEvent event) {
+      if (myGameMode == currentGameMode)
+         keyListen(event);
+   }
+
+   public void checkMouseListenTrigger(String currentGameMode, MouseEvent event) {
+      if (myGameMode == currentGameMode)
+         mouseListen(event);
+   }
 }
