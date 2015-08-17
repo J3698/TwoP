9e05738be72d40aa5f0c1c032bd7a29209ad9518
Antioch Sanders <antiochsanders@gmail.com>
2015-08-16 21:52:01 -0400

    -MainMenu.java recreated, takes mouse input

--- twop/gamestate/MainMenu.java
+++ twop/gamestate/MainMenu.java
@@ -2,35 +2,39 @@ package twop.gamestate;
 
 import twop.Game;
 import twop.util.StringDraw;
-import twop.gui.*;
+import twop.util.Vector2;
+import twop.gui.MenuButton;
+import twop.gui.GUIManager;
 
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 
-public class MainMenu implements GameState {
-   private String myGameMode = "mainMenu";
+public class MainMenu extends GameState {
    private Game myGame;
    private int myGameWidth;
    private int myGameHeight;
    private GUIManager myGUIManager;
 
    public MainMenu(Game game, int gameWidth, int gameHeight) {
+      super("mainmenu");
       myGame = game;
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
       myGUIManager = new GUIManager();
-      myGUIManager.addButton(new MenuButton());
+      myGUIManager.addButton(new MenuButton(new PlayListener(), "PLAY", new Vector2(200, 20), new Vector2(240, 100), myGameWidth, myGameHeight));
+      myGUIManager.addButton(new MenuButton(new InstructionsListener(), "HOW-TO", new Vector2(200, 130), new Vector2(240, 100), myGameWidth, myGameHeight));
+      myGUIManager.addButton(new MenuButton(new OptionsListener(), "OPTIONS", new Vector2(200, 240), new Vector2(240, 100), myGameWidth, myGameHeight));
+      myGUIManager.addButton(new MenuButton(new CreditsListener(), "CREDITS", new Vector2(200, 350), new Vector2(240, 100), myGameWidth, myGameHeight));
    }
 
    public void draw(Graphics pen) {
-      pen.setColor(Color.white);
+      pen.setColor(new Color(100, 100, 100));
       pen.fillRect(0, 0, myGameWidth, myGameHeight);
-      pen.setColor(Color.black);
-      StringDraw.drawStringCenter(pen, "TwoP", myGameWidth / 2, 60);
-      StringDraw.drawStringCenter(pen, "Play", myGameWidth / 2, 100);
-      StringDraw.drawStringCenter(pen, "Instructions", myGameWidth / 2, 140);
-      StringDraw.drawStringCenter(pen, "Credits", myGameWidth / 2, 180);
+      myGUIManager.draw(pen);
    }
 
    public void update() {
@@ -39,16 +43,31 @@ public class MainMenu implements GameState {
    public void keyListen(KeyEvent event) {
    }
 
-   public void checkUpdateTrigger(String currentGameMode) {
-      if (myGameMode == currentGameMode)
-         update();
+   public void mouseListen(MouseEvent event) {
+      myGUIManager.mouseListen(event);
    }
-   public void checkDrawTrigger(String currentGameMode, Graphics pen) {
-      if (myGameMode == currentGameMode)
-         draw(pen);
+
+   private class PlayListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         myGame.setGameMode("play");
+      }
    }
-   public void checkKeyListenTrigger(String currentGameMode, KeyEvent event) {
-      if (myGameMode == currentGameMode)
-         keyListen(event);
+
+   private class InstructionsListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         myGame.setGameMode("instructions");
+      }
+   }
+
+   private class OptionsListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         myGame.setGameMode("options");
+      }
+   }
+
+   private class CreditsListener implements ActionListener {
+      public void actionPerformed(ActionEvent e) {
+         myGame.setGameMode("credits");
+      }
    }
 }
