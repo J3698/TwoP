8b1f36c8bbfb38a5a89636356a5cb6308e16b2cc
Antioch Sanders <antiochsanders@gmail.com>
2015-08-17 19:24:13 -0400

    -Much of MenuButton.java's responsibilities given to Button

--- twop/gui/MenuButton.java
+++ twop/gui/MenuButton.java
@@ -10,25 +10,10 @@ import java.awt.Color;
 import java.util.Random;
 
 public class MenuButton extends Button {
-   private static Font menuFont = StringDraw.menuFont();
-   private String myText;
-   private Color myColor;
    public MenuButton(ActionListener listener, String text, Vector2 position, Vector2 size, int gameWidth, int gameHeight) {
-      super(listener, position, size, gameWidth, gameHeight);
-      myText = text;
+      super(listener, text, Color.black, Color.black, StringDraw.menuFont(), position, size, gameWidth, gameHeight);
       Random r = new Random();
-      myColor = new Color(150 + r.nextInt(50), 150 + r.nextInt(50), 150 + r.nextInt(50));
-   }
-
-   public void draw(Graphics pen) {
-      pen.setColor(myColor);
-      int x = (int) getPosition().getX();
-      int y = (int) getPosition().getY();
-      int width = (int) getSize().getX();
-      int height = (int) getSize().getY();
-      pen.fillRect(x, y, width, height);
-      pen.setColor(new Color(100, 100, 100, 100));
-      pen.setFont(menuFont);
-      StringDraw.drawStringCenter(pen, myText, x + width / 2, y + height / 2);
+      setBodyColor(new Color(150 + r.nextInt(50), 150 + r.nextInt(50), 150 + r.nextInt(50)));
+      setTextColor(new Color(100, 100, 100));
    }
 }
