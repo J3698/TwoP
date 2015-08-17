c6e4106b1ba4645ad819aab58a2185ba03b0c74a
Antioch Sanders <antiochsanders@gmail.com>
2015-08-16 21:58:38 -0400

    Button.java implemented
    Button
    -Import changes
    -Position/size no longer represented as ratios
    -Buttons have an actionListener
    -Constructor changes
    -Method for determining if button is clicked

--- twop/gui/Button.java
+++ twop/gui/Button.java
@@ -2,22 +2,37 @@ package twop.gui;
 
 import twop.util.Vector2;
 
+import java.awt.event.ActionListener;
 import java.awt.Graphics;
 
 public abstract class Button {
+   private ActionListener myListener;
    private int myGameWidth;
    private int myGameHeight;
-   private Vector2 myPositionRatio;
-   private Vector2 mySizeRatio;
+   private Vector2 myPosition;
+   private Vector2 mySize;
 
-   public Button(int gameWidth, int gameHeight, Vector2 positionRatio,
-                                                    Vector2 sizeRatio) {
+   public Button(ActionListener listener, Vector2 position, Vector2 size,
+                                            int gameWidth, int gameHeight) {
+      myListener = listener;
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
-      myPositionRatio = positionRatio;
-      mySizeRatio = sizeRatio;
+      myPosition = position;
+      mySize = size;
    }
 
    public abstract void draw(Graphics pen);
 
+   public void doAction() {
+      myListener.actionPerformed(null);
+   }
+
+   public boolean collidesPoint(int x, int y) {
+      boolean inXBounds = (x > myPosition.getX() && x < myPosition.getX() + mySize.getX());
+      boolean inYBounds = (y > myPosition.getY() && y < myPosition.getY() + mySize.getY());
+      return (inXBounds && inYBounds);
+   }
+
+   public Vector2 getPosition() { return myPosition; }
+   public Vector2 getSize() { return mySize; }
 }
