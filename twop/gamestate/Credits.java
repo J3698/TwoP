8042bef34f54a9c3bf5a3444faa3ab3597fa9376
J3698 <antiochsanders@gmail.com>
2015-08-18 15:27:10 -0400

    -GUI to exit Credits added to Credits.java

--- twop/gamestate/Credits.java
+++ twop/gamestate/Credits.java
@@ -1,9 +1,15 @@
 package twop.gamestate;
 
 import twop.Game;
+import twop.gamestate.Options.MainMenuListener;
+import twop.gui.GUIManager;
+import twop.gui.InstructionsButton;
 import twop.util.StringDraw;
+import twop.util.Vector2;
 
 import java.util.Random;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
@@ -19,6 +25,7 @@ public class Credits extends GameState {
    private Color myColor;
    private KeyAdapter myKeyListener;
    private MouseAdapter myMouseListener;
+   private GUIManager myGUIManager;
 
    public Credits(Game game, int gameWidth, int gameHeight) {
       super("credits");
@@ -29,6 +36,8 @@ public class Credits extends GameState {
       myKeyListener = new KeyListener();
       Random r = new Random();
       myColor = new Color(150 + r.nextInt(50), 150 + r.nextInt(50), 150 + r.nextInt(50));
+      myGUIManager = new GUIManager();
+      myGUIManager.addButton(new InstructionsButton(new MainMenuListener(), "Main menu", new Vector2(20, 420), myGameWidth, myGameHeight));
    }
 
    public void draw(Graphics pen) {
@@ -41,23 +50,31 @@ public class Credits extends GameState {
       StringDraw.drawStringCenter(pen, "Sound: www.freesfx.co.uk", halfWidth, quarterHeight);
       StringDraw.drawStringCenter(pen, "Support: All of TwoP's players!", halfWidth, 2 * quarterHeight);
       StringDraw.drawStringCenter(pen, "Other stuff: Antioch Sanders", halfWidth, 3 * quarterHeight);
+      myGUIManager.draw(pen);
    }
 
    public void update() {
    }
 
-   public void mouseListen(MouseEvent event) {}
-   public void keyListen(KeyEvent event) {}
+   public class MainMenuListener implements ActionListener {
+	   public void actionPerformed(ActionEvent event) {
+		   myGame.setGameMode("mainmenu");
+	   }
+   }
 
    public KeyAdapter getKeyListener() { return myKeyListener; }
    public MouseAdapter getMouseListener() { return myMouseListener; }
 
    private class KeyListener extends KeyAdapter {
-	   
    }
 
    private class MouseListener extends MouseAdapter {
-	   
+	   public void mousePressed(MouseEvent event) {
+		   myGUIManager.mousePressed(event);
+	   }
+	   public void mouseMoved(MouseEvent event) {
+		   myGUIManager.mouseMoved(event);
+	   }
    }
 }
 
