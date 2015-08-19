b77f4fccb449c6295ba05d34357d64a2b566acf3
J3698 <antiochsanders@gmail.com>
2015-08-19 01:27:58 -0400

    Changes to GameOverButton.java
    -StringDraw import added
    -Call to super changed
    -Methods draw and drawHovered over-ridden

--- twop/gui/GameOverButton.java
+++ twop/gui/GameOverButton.java
@@ -5,19 +5,28 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 
+import twop.util.StringDraw;
 import twop.util.Vector2;
 
 public class GameOverButton extends Button {
 	public GameOverButton(ActionListener listener, String text, Vector2 position, int gameWidth, int gameHeight) {
-		super(listener, text, new Color(0, 0, 0, 0), new Color(150, 150, 150), new Font("Ariel", 0, 20),
-												   position, new Vector2(70, 30), gameWidth, gameHeight);
+		super(listener, text, new Color(0, 0, 0, 0), new Color(150, 150, 150), new Font("Ariel", 1, 25),
+												   position, new Vector2(110, 80), gameWidth, gameHeight);
 	}
 
 	public void draw(Graphics pen) {
-		
+		pen.setFont(new Font("sans", 0, 15));
+		pen.setColor(getTextColor());
+		int centerX = (int) (getPosition().getX() + getSize().getX() / 2);
+		int centerY = (int) (getPosition().getY() + getSize().getY() / 2);
+		StringDraw.drawStringCenter(pen, getText(), centerX, centerY);
 	}
 
 	public void drawHovered(Graphics pen) {
-		
+		pen.setFont(new Font("sans", 0, 15));
+		pen.setColor(new Color(180, 180, 180));
+		int centerX = (int) (getPosition().getX() + getSize().getX() / 2);
+		int centerY = (int) (getPosition().getY() + getSize().getY() / 2);
+		StringDraw.drawStringCenter(pen, getText(), centerX, centerY);
 	}
 }
