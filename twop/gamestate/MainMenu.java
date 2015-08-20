53c69f2fe98e652d9751d7515ac097b8a1c57a63
Antioch Sanders <antiochsanders@gmail.com>
2015-08-20 09:19:02 -0400

    -Support for removal of Game.java in MainMenu

--- twop/gamestate/MainMenu.java
+++ twop/gamestate/MainMenu.java
@@ -1,6 +1,6 @@
 package twop.gamestate;
 
-import twop.Game;
+import twop.GamePanel;
 import twop.util.StringDraw;
 import twop.util.Vector2;
 import twop.gui.MenuButton;
@@ -16,21 +16,21 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class MainMenu extends GameState {
-   private Game myGame;
+   private GamePanel myGamePanel;
    private int myGameWidth;
    private int myGameHeight;
    private GUIManager myGUIManager;
    private KeyAdapter myKeyListener;
    private MouseAdapter myMouseListener;
 
-   public MainMenu(Game game, int gameWidth, int gameHeight) {
+   public MainMenu(GamePanel gamePanel, int gameWidth, int gameHeight) {
       super("mainmenu");
-      myGame = game;
+      myGamePanel = gamePanel;
       myGameWidth = gameWidth;
       myGameHeight = gameHeight;
       myMouseListener = new MouseListener();
       myKeyListener = new KeyListener();
-      myGUIManager = new GUIManager();                                            //240  25
+      myGUIManager = new GUIManager(myGamePanel);
       myGUIManager.addButton(new MenuButton(new PlayListener(), "PLAY", new Vector2(200, 20), new Vector2(240, 100), myGameWidth, myGameHeight));
       myGUIManager.addButton(new MenuButton(new InstructionsListener(), "HOW-TO", new Vector2(200, 130), new Vector2(240, 100), myGameWidth, myGameHeight));
       myGUIManager.addButton(new MenuButton(new OptionsListener(), "OPTIONS", new Vector2(200, 240), new Vector2(240, 100), myGameWidth, myGameHeight));
