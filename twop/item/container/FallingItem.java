5bae960a95ae6ee574ae8798e38c8ca621934b09
Antioch Sanders <antiochsanders@gmail.com>
2015-08-22 01:32:23 -0400

    -FallingItem.java ItemContainer created

---
+++ twop/item/container/FallingItem.java
@@ -0,0 +1,10 @@
+package twop.item.container;
+
+import twop.item.Item;
+import twop.util.Vector2;
+
+public class FallingItem extends ItemContainer {
+   public FallingItem(Item item, Vector2 position) {
+      super(item, position);
+   }
+}
