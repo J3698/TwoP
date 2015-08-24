9869835b91053cc77520314ee360303f0d8c5a0c
Antioch Sanders <antiochsanders@gmail.com>
2015-08-24 00:10:38 -0400

    -lifeSpan and tick implemented in Weather.java

--- twop/weather/Weather.java
+++ twop/weather/Weather.java
@@ -5,15 +5,22 @@ import twop.GamePanel;
 import java.awt.Graphics;
 
 public abstract class Weather {
+   private int myLife;
+   private int myTick;
    private GamePanel myGamePanel;
 
-   public Weather(GamePanel gamePanel) {
+   public Weather(int life, GamePanel gamePanel) {
+      myLife = life;
       myGamePanel = gamePanel;
    }
 
    public abstract void update();
    public abstract void draw(Graphics pen);
-// public abstract void fade();
 
    public GamePanel getGamePanel() { return myGamePanel; }
+   public int getLife() { return myLife; }
+   public void setLife(int life) { myLife = life; }
+   public boolean isOver() { return (myLife <= myTick); }
+   public void tick() { myTick++; }
+   public int getTick() { return myTick; }
 }
