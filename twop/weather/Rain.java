9ad40788ef45b57f68a5a217cbdc94a633c07e1b
Antioch Sanders <antiochsanders@gmail.com>
2015-08-25 00:02:57 -0400

    -Rain smoother

--- twop/weather/Rain.java
+++ twop/weather/Rain.java
@@ -18,32 +18,42 @@ public class Rain extends Weather {
    }
 
    public void update() {
-      int xPos;
-      for (int i = 0; i < 7; i++) {
-         xPos = new Random().nextInt(getGamePanel().getGameWidth());
-         myRainParticles.add(new RainParticle(new Vector2(xPos, 0)));
-      }
-      for (int i = 0; i < myRainParticles.size(); i++) {
-         myRainParticles.get(i).update();
-      }
+      if (getTick() < 2 * 150) {
+      } else if (getTick() < getLife() - 2 * 150) {
+         int xPos;
+         for (int i = 0; i < 7; i++) {
+            xPos = new Random().nextInt(getGamePanel().getGameWidth());
+            myRainParticles.add(new RainParticle(new Vector2(xPos, 0)));
+         }
+         for (int i = 0; i < myRainParticles.size(); i++) {
+            myRainParticles.get(i).update();
+         }
 
-      Player firstPlayer = getGamePanel().getPlay().getFirstPlayer();
-      Player secondPlayer = getGamePanel().getPlay().getSecondPlayer();
-      if (firstPlayer.hasEffectKey("fire")) {
-         firstPlayer.getEffects().get("fire").setLife(0);
-      }
-      if (secondPlayer.hasEffectKey("fire")) {
-         secondPlayer.getEffects().get("fire").setLife(0);
-      }
-
+         Player firstPlayer = getGamePanel().getPlay().getFirstPlayer();
+         Player secondPlayer = getGamePanel().getPlay().getSecondPlayer();
+         if (firstPlayer.hasEffectKey("fire")) {
+            firstPlayer.getEffects().get("fire").setLife(0);
+         }
+         if (secondPlayer.hasEffectKey("fire")) {
+            secondPlayer.getEffects().get("fire").setLife(0);
+         }
+      } else {}
       tick();
    }
 
    public void draw(Graphics pen) {
-      pen.setColor(new Color(0, 0, 0, 150));
-      pen.fillRect(0, 0, getGamePanel().getGameWidth(), getGamePanel().getGameHeight());
-      for (int i = 0; i < myRainParticles.size(); i++) {
-         myRainParticles.get(i).draw(pen);
+      if (getTick() < 2 * 150) {
+         pen.setColor(new Color(0, 0, 0, getTick() / 2));
+         pen.fillRect(0, 0, getGamePanel().getGameWidth(), getGamePanel().getGameHeight());
+      } else if (getTick() < getLife() - 2 * 150) {
+         pen.setColor(new Color(0, 0, 0, 150));
+         pen.fillRect(0, 0, getGamePanel().getGameWidth(), getGamePanel().getGameHeight());
+         for (int i = 0; i < myRainParticles.size(); i++) {
+            myRainParticles.get(i).draw(pen);
+         }
+      } else {
+         pen.setColor(new Color(0, 0, 0, (getLife() - getTick()) / 2));
+         pen.fillRect(0, 0, getGamePanel().getGameWidth(), getGamePanel().getGameHeight());
       }
    }
 
