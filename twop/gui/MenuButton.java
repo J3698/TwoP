d4d6d9e81e1c180483c16d75bc05b40a7c5eb4e3
Antioch Sanders <antiochsanders@gmail.com>
2015-08-29 00:57:09 -0400

    MenuButton color changed

--- twop/gui/MenuButton.java
+++ twop/gui/MenuButton.java
@@ -11,7 +11,9 @@ public class MenuButton extends Button {
    public MenuButton(ActionListener listener, String text, Vector2 position, Vector2 size, int gameWidth, int gameHeight) {
       super(listener, text, Color.black, Color.black, StringDraw.menuFont(), position, size, gameWidth, gameHeight);
       Random r = new Random();
-      setBodyColor(new Color(190 + r.nextInt(50), 190 + r.nextInt(50), 190 + r.nextInt(50)));
+      int base = 100;
+      int rand = 255 - base;
+      setBodyColor(new Color(base + r.nextInt(rand), base + r.nextInt(rand), base + r.nextInt(rand)));
       setTextColor(new Color(100, 100, 100));
    }
 }
