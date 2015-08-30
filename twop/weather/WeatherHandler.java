3450d19415ed2b4a4221c13b632bd2d195150811
Antioch Sanders <antiochsanders@gmail.com>
2015-08-30 00:01:01 -0400

    (See last commit)

--- twop/weather/WeatherHandler.java
+++ twop/weather/WeatherHandler.java
@@ -9,7 +9,6 @@ import java.util.Random;
 public class WeatherHandler {
    private GamePanel myGamePanel;
    private Weather myCurrentWeather;
-   private Sound mySound = new Sound("rain");
 
    public WeatherHandler(GamePanel gamePanel) {
       myGamePanel = gamePanel;
@@ -17,13 +16,10 @@ public class WeatherHandler {
 
    public void update() {
       if (myCurrentWeather != null) {
-         if (myCurrentWeather.getTick() == 200)
-            mySound.loop();
          myCurrentWeather.update();
          if (myCurrentWeather.isOver()) {
             myCurrentWeather = null;
             myGamePanel.getPlay().getPlaneHandler().enableFire();
-            mySound.stop();
          }
       } else {
          if (new Random().nextDouble() < 0.002) {
