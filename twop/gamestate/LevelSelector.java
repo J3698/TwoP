23d3e3a45270c58928c28c617b3ac5a52393c9f6
Antioch Sanders <antiochsanders@gmail.com>
2015-09-03 14:59:08 -0400

    Changes to LevelSelector
    -import changes
    -init function for starting
    -commented lines deleted
    -draws background
    -camera fixing moved to fixCamera function

--- twop/gamestate/LevelSelector.java
+++ twop/gamestate/LevelSelector.java
@@ -2,17 +2,13 @@ package twop.gamestate;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.color.ColorSpace;
+import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.Vector;
 
 import twop.GamePanel;
 import twop.Player;
-import twop.effect.FireEffect;
-import twop.gui.GUIManager;
 import twop.util.Vector2;
 
 public class LevelSelector extends GameState {
@@ -34,29 +30,26 @@ public class LevelSelector extends GameState {
       myKeyListener = new KeyListener();
       myMouseListener = new MouseListener();
       myPlayer = new Player(new Vector2(100, 50), 19, 0, myGameHeight - 1, myGameWidth, 0);
+   public void init() {
       myPlayer.getControls().setSecondControls();
       myGamePanel.getCamera().setImageSize(1000, 1000);
-      //Not sure lines of code, do I have to change gameWidth/Height?
-//      myGamePanel.setGameWidth(1000);
-//      myGamePanel.setGameHeight(1000);
    }
 
    @Override
    public void draw(Graphics pen) {
-      Vector2 center = myPlayer.getCenter().copy();
-      Vector2 newPos1 = myPlayer.getCenter().copy();
-      Vector2 newPos2 = myPlayer.getCenter().copy();
-      Vector2 half = new Vector2(myGamePanel.getGameWidth() / 2, myGamePanel.getGameHeight() / 2);
-      center.subtractVector(half);
-      myGamePanel.getCamera().reset();
-      //Can't assume pos1 and 2 are correct, we have to completely overwrite them, not subtract from them.
-      myGamePanel.getCamera().setPos1(newPos1);
-      myGamePanel.getCamera().getPos1().subtractVector(half);
-      myGamePanel.getCamera().setPos2(newPos2);
-      myGamePanel.getCamera().getPos2().addVector(half);
+      fixCamera();
+      pen.setColor(Color.white);
+      pen.fillRect(0, 0, 1000, 1000);
 
-      pen.setColor(Color.red);
-      pen.drawRect(0, 0, 100, 100);
+      pen.setColor(Color.CYAN);
+      for (int x = 0; x < 10; x++) {
+         pen.fillRect(x * 100, 0, 50, 1000);
+      }
+      pen.setColor(Color.BLUE);
+      for (int y = 0; y < 100; y++) {
+         pen.fillRect(0, y * 100, 1000, 50);
+      }
+
       myPlayer.draw(pen);
    }
 
@@ -65,7 +58,14 @@ public class LevelSelector extends GameState {
       myPlayer.update();
    }
 
-
+   public void fixCamera() {
+      Vector2 newPos1 = myPlayer.getCenter().copy();
+      Vector2 newPos2 = myPlayer.getCenter().copy();
+      Vector2 half = new Vector2(myGamePanel.getGameWidth() / 2, myGamePanel.getGameHeight() / 2);
+      newPos1.subtractVector(half);
+      newPos2.addVector(half);
+      myGamePanel.getCamera().setPerspective(newPos1, newPos2);
+   }
    
    
    
