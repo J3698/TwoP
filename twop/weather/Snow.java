cdefcccc35eb1dc49fa9f8b14f4fc85412e17c7f
J3698 <antiochsanders@gmail.com>
2015-09-09 01:11:29 -0400

    Snow weather shell added

---
+++ twop/weather/Snow.java
@@ -0,0 +1,29 @@
+package twop.weather;
+
+import java.awt.Graphics;
+
+import twop.GamePanel;
+
+public class Snow extends Weather {
+
+   public Snow(int life, GamePanel gamePanel) {
+      super(life, gamePanel);
+   }
+
+   @Override
+   public void update() {
+   }
+
+   @Override
+   public void draw(Graphics pen) {
+      // TODO Auto-generated method stub
+
+   }
+
+   @Override
+   public void close() {
+      // TODO Auto-generated method stub
+
+   }
+
+}
