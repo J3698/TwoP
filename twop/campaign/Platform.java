65a0ae1a78f332239f5cc1a056c5082f465bfa7c
J3698 <antiochsanders@gmail.com>
2015-09-09 01:10:54 -0400

    Platform.java created

---
+++ twop/campaign/Platform.java
@@ -0,0 +1,21 @@
+package twop.campaign;
+
+import twop.Rect;
+import twop.physics.PhysicsObject;
+import twop.physics.PhysicsRect;
+import twop.util.Vector2;
+
+public class Platform extends Rect {
+   private PhysicsRect myPhysics;
+
+   public Platform(Vector2 position, double width, double height) {
+      super(position, width, height);
+      myPhysics = new PhysicsRect();
+      myPhysics.setAnimate(false);
+      myPhysics.setSolid(true);
+   }
+
+   public PhysicsObject getPhysics() {
+      return myPhysics;
+   }
+}
