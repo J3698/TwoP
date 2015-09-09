bea64dac8ed14f6004f4909923c49617932fcdf7
J3698 <antiochsanders@gmail.com>
2015-09-09 01:11:42 -0400

    RainParticle.java added

---
+++ twop/particlesys/particle/RainParticle.java
@@ -0,0 +1,30 @@
+package twop.particlesys.particle;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import twop.util.Vector2;
+
+public class RainParticle extends Particle {
+   private static int floor = 480;
+
+   public RainParticle(Vector2 position, Vector2 velocity) {
+      super(position, velocity);
+   }
+
+   @Override
+   public void update() {
+      getParticlePosition().addVector(getParticleVelocity());
+   }
+
+   @Override
+   public void draw(Graphics pen) {
+      int x = (int) getParticlePosition().getX();
+      int y = (int) getParticlePosition().getY();
+      for (int i = 0; i < 5; i++) {
+         pen.setColor(new Color(0, 0, 255, 50 * (i + 1)));
+         pen.drawLine(x, y + 2 * i, x, y + 2 * i + 2);
+      }
+   }
+
+}
