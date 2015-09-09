0938de4e46ecc66d338952846e10380d59563f71
J3698 <antiochsanders@gmail.com>
2015-09-09 01:12:03 -0400

    Changes to ParticleSystem.java

--- twop/particlesys/ParticleSystem.java
+++ twop/particlesys/ParticleSystem.java
@@ -1,15 +1,20 @@
 package twop.particlesys;
 
-import twop.util.Vector2;
-import twop.particlesys.particle.*;
-
+import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.Random;
-import java.awt.Graphics;
+
+import twop.particlesys.particle.FireParticle;
+import twop.particlesys.particle.Particle;
+import twop.particlesys.particle.PoisonParticle;
+import twop.particlesys.particle.RainParticle;
+import twop.util.Vector2;
 
 public class ParticleSystem {
    private ArrayList<Particle> myParticles;
+
    private Vector2 mySourcePosition;
+   private Vector2 myMaxSourceOffset;;
    private Vector2 myInitialVelocity = new Vector2(0, 0);
    private String myParticleType;
    private int myEmissionRate = 1;
@@ -17,7 +22,7 @@ public class ParticleSystem {
    private int myUpperAngleLimit = 180;
 
    public ParticleSystem(String particleType, Vector2 position, Vector2 velocity,
-                                        int lowerAngleLimit, int upperAngleLimit) {
+         int lowerAngleLimit, int upperAngleLimit) {
       myParticleType = particleType;
       mySourcePosition = position;
       myInitialVelocity = velocity;
@@ -29,15 +34,26 @@ public class ParticleSystem {
    public ParticleSystem(String particleType, Vector2 position) {
       myParticleType = particleType;
       mySourcePosition = position;
+      myParticles = new ArrayList<Particle>();
+      parseParticleType();
+   }
+
+   public void parseParticleType() {
       if (myParticleType.equals("fire")) {
+         myMaxSourceOffset = new Vector2(30, 30);
          myInitialVelocity = new Vector2(0, -3);
          myLowerAngleLimit = -40;
          myUpperAngleLimit = 40;
       } else if (myParticleType.equals("poison")) {
+         myMaxSourceOffset = new Vector2(30, 30);
+      } else if (myParticleType.equals("rain")) {
+         myMaxSourceOffset = new Vector2(640, 0);
+         myInitialVelocity = new Vector2(0, 10);
+         myLowerAngleLimit = -1;
+         myUpperAngleLimit = 1;
       } else {
          System.out.println("Unknown Particle Type");
       }
-      myParticles = new ArrayList<Particle>();
    }
 
    public void update() {
@@ -61,20 +77,33 @@ public class ParticleSystem {
 
    public void createParticles() {
       Vector2 position = mySourcePosition.copy();
+      if (myMaxSourceOffset != null) {
+         double xLowerLim = -(myMaxSourceOffset.getX() / 2);
+         double yLowerLim = -(myMaxSourceOffset.getY() / 2);
+         double xUpperLim = myMaxSourceOffset.getX() / 2;
+         double yUpperLim = myMaxSourceOffset.getY() / 2;
+         position.addVector(Vector2.randVector(xLowerLim, yLowerLim, xUpperLim, yUpperLim));
+      }
+
       Vector2 velocity = myInitialVelocity.copy();
+
       int angle = myLowerAngleLimit + new Random().nextInt(myUpperAngleLimit - myLowerAngleLimit);
       velocity.rotateDegrees(angle);
+
       if (myParticleType.equalsIgnoreCase("poison")) {
          if (new Random().nextDouble() < 0.01)
             myParticles.add(new PoisonParticle(position, velocity));
-      }
-      else if (myParticleType.equalsIgnoreCase("fire")) {
+      } else if (myParticleType.equalsIgnoreCase("fire")) {
          myParticles.add(new FireParticle(position, velocity));
+      } else if (myParticleType.equalsIgnoreCase("rain")) {
+         myParticles.add(new RainParticle(position, velocity));
       }
    }
 
    public Vector2 getSourcePosition() { return mySourcePosition; }
    public void setSourcePosition(Vector2 position) { mySourcePosition = position; }
+   public void setSourceOffset(Vector2 offset) { myMaxSourceOffset = offset; }
+   private Vector2 getSourceOffset() { return myMaxSourceOffset; }
    public void setEmissionRate(int emissionRate) { myEmissionRate = emissionRate; }
    public void setEmissionAngles(int lowerLimit, int upperLimit) {
       myLowerAngleLimit = lowerLimit;
