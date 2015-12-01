71270e2df0d0e4e3e19df0be0b522bc99d0ae889
J3698 <antiochsanders@gmail.com>
2015-11-30 21:09:49 -0500

    Changes to ParticleSystem.java
    -Angle limits in radians
    -parseParticleType now private
    -ORP
    -setEmissionAngles(double angle) added
    -main method added for testing particles

--- twop/particlesys/ParticleSystem.java
+++ twop/particlesys/ParticleSystem.java
@@ -1,9 +1,18 @@
 package twop.particlesys;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import twop.particlesys.particle.AsteroidParticle;
 import twop.particlesys.particle.FireParticle;
 import twop.particlesys.particle.Particle;
 import twop.particlesys.particle.PoisonParticle;
@@ -18,11 +27,11 @@ public class ParticleSystem {
    private Vector2 myInitialVelocity = new Vector2(0, 0);
    private String myParticleType;
    private int myEmissionRate = 1;
-   private int myLowerAngleLimit = -180;
-   private int myUpperAngleLimit = 180;
+   private double myLowerAngleLimit = -Math.PI;
+   private double myUpperAngleLimit = Math.PI;
 
    public ParticleSystem(String particleType, Vector2 position, Vector2 velocity,
-         int lowerAngleLimit, int upperAngleLimit) {
+         double lowerAngleLimit, double upperAngleLimit) {
       myParticleType = particleType;
       mySourcePosition = position;
       myInitialVelocity = velocity;
@@ -38,19 +47,23 @@ public class ParticleSystem {
       parseParticleType();
    }
 
-   public void parseParticleType() {
+   private void parseParticleType() {
       if (myParticleType.equals("fire")) {
          myMaxSourceOffset = new Vector2(30, 30);
          myInitialVelocity = new Vector2(0, -3);
-         myLowerAngleLimit = -40;
-         myUpperAngleLimit = 40;
+         myLowerAngleLimit = -Math.toRadians(40);
+         myUpperAngleLimit = Math.toRadians(40);
+
       } else if (myParticleType.equals("poison")) {
          myMaxSourceOffset = new Vector2(30, 30);
+
       } else if (myParticleType.equals("rain")) {
          myMaxSourceOffset = new Vector2(640, 0);
          myInitialVelocity = new Vector2(0, 10);
-         myLowerAngleLimit = -1;
-         myUpperAngleLimit = 1;
+         myLowerAngleLimit = -Math.toRadians(1);
+         myUpperAngleLimit = Math.toRadians(1);
+
+      } else if (myParticleType.equals("asteroid")) {
       } else {
          System.out.println("Unknown Particle Type");
       }
@@ -60,10 +73,11 @@ public class ParticleSystem {
       Particle temp;
       for (int i = 0; i < myParticles.size(); i++) {
          temp = myParticles.get(i);
-         if(temp.isDead())
+         if (temp.isDead()) {
             myParticles.remove(temp);
-         else
+         } else {
             temp.update();
+         }
       }
       for (int i = 0; i < myEmissionRate; i++) {
          createParticles();
@@ -71,8 +85,9 @@ public class ParticleSystem {
    }
 
    public void draw(Graphics pen) {
-      for (int i = 0; i < myParticles.size(); i++)
+      for (int i = 0; i < myParticles.size(); i++) {
          myParticles.get(i).draw(pen);
+      }
    }
 
    public void createParticles() {
@@ -82,21 +97,26 @@ public class ParticleSystem {
          double yLowerLim = -(myMaxSourceOffset.getY() / 2);
          double xUpperLim = myMaxSourceOffset.getX() / 2;
          double yUpperLim = myMaxSourceOffset.getY() / 2;
-         position.addVector(Vector2.randVector(xLowerLim, yLowerLim, xUpperLim, yUpperLim));
+         position.addVector(Vector2.randSquareVector(xLowerLim, yLowerLim, xUpperLim, yUpperLim));
       }
 
       Vector2 velocity = myInitialVelocity.copy();
 
-      int angle = myLowerAngleLimit + new Random().nextInt(myUpperAngleLimit - myLowerAngleLimit);
-      velocity.rotateDegrees(angle);
+      double angle = myLowerAngleLimit + new Random().nextDouble()
+            * (myUpperAngleLimit - myLowerAngleLimit);
+      velocity.rotateRadians(angle);
+      System.out.println(angle);
 
       if (myParticleType.equalsIgnoreCase("poison")) {
-         if (new Random().nextDouble() < 0.01)
+         if (new Random().nextDouble() < 0.01) {
             myParticles.add(new PoisonParticle(position, velocity));
+         }
       } else if (myParticleType.equalsIgnoreCase("fire")) {
          myParticles.add(new FireParticle(position, velocity));
       } else if (myParticleType.equalsIgnoreCase("rain")) {
          myParticles.add(new RainParticle(position, velocity));
+      } else if (myParticleType.equalsIgnoreCase("asteroid")) {
+         myParticles.add(new AsteroidParticle(position, velocity));
       }
    }
 
@@ -105,12 +125,50 @@ public class ParticleSystem {
    public void setSourceOffset(Vector2 offset) { myMaxSourceOffset = offset; }
    @SuppressWarnings("unused")
    private Vector2 getSourceOffset() { return myMaxSourceOffset; }
+
    public void setEmissionRate(int emissionRate) { myEmissionRate = emissionRate; }
-   public void setEmissionAngles(int lowerLimit, int upperLimit) {
+   public void setEmissionAngles(double lowerLimit, double upperLimit) {
       myLowerAngleLimit = lowerLimit;
       myUpperAngleLimit = upperLimit;
    }
-   public int[] getEmissionAngles() { return new int[]{myLowerAngleLimit, myUpperAngleLimit}; }
+   public void setEmissionAngles(double angle) { setEmissionAngles(angle, angle); }
+   public double[] getEmissionAngles() { return new double[]{ myLowerAngleLimit, myUpperAngleLimit }; }
    public void setInitialVelocity(Vector2 velocity) { myInitialVelocity = velocity; }
    public Vector2 getInitialVelocity() { return myInitialVelocity; }
+
+
+
+   public static void main(String[] args) {
+      JFrame frame = new JFrame("Particle Tester");
+      frame.setSize(600, 600);
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(false);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new ParticlePanel());
+      frame.setVisible(true);
+   }
+}
+
+class ParticlePanel extends JPanel {
+   private static final long serialVersionUID = 8683345521292327079L;
+   private int refresh = 60;
+
+   public ParticlePanel() {
+      setFocusable(true);
+      addMouseListener(new MouseAdapter() {});
+      new Timer(refresh, new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent event) {
+            update();
+            repaint();
+         }}).start();
+   }
+
+   @Override
+   public void paintComponent(Graphics pen) {
+      pen.setColor(Color.black);
+      pen.fillRect(-1, -1, 601, 601);
+   }
+   public void update() {
+   }
 }
