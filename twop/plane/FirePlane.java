0002cf92935ba13f7aedb089e99d4c1f1d69bf78
J3698 <antiochsanders@gmail.com>
2015-11-30 21:04:50 -0500

    -ORP to FirePlane.java

--- twop/plane/FirePlane.java
+++ twop/plane/FirePlane.java
@@ -1,21 +1,23 @@
 package twop.plane;
 
-import twop.util.Vector2;
+import java.awt.Color;
+
 import twop.Player;
 import twop.effect.FireEffect;
-
-import java.awt.Color;
+import twop.util.Vector2;
 
 public class FirePlane extends Plane {
    public FirePlane(Vector2 position, double width, double height,
-                                       Color background, int spot) {
+         Color background, int spot) {
       super(position, width, height, background, Color.red, spot);
    }
 
+   @Override
    public void onCollision(Player player) {
-      if (player.hasEffectKey("fire"))
+      if (player.hasEffectKey("fire")) {
          player.getEffects().get("fire").increaseIntensity();
-      else
+      } else {
          player.addEffect("fire", new FireEffect(player));
+      }
    }
 }
